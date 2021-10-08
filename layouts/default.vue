4975dccbf63686626cda51b33a48cf013b15942e
Crisel <riocrisel18@gmail.com>
2021-10-09 03:28:45 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <div class="main-wrapper">
+      <partials-navbar />
+      <partials-sidebar />
+
+      <!-- Main Content -->
+      <div class="main-content">
+        <section class="section">
+          <div class="section-header">
+            <h1>Blank Page</h1>
+          </div>
+
+          <div class="section-body">
+          </div>
+        </section>
+      </div>
+      <partials-footer />
+    </div>
+  </div>
+</template>
